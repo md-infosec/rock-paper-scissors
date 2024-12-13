65589d5d56b8cacbcebe0d68cb34f2a0e3edd7f2
md-infosec <85002000+md-infosec@users.noreply.github.com>
2024-12-13 12:32:29 -0500

    In script.js, added a function to generate a random number, scale it, and then choose rock, paper, or scissors based on the number

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    let randomNum = Math.random(1, 3);
+    let scaledNum = Math.floor(randomNum * 3) + 1;
+    
+    if (scaledNum == 1) {
+        return "rock";
+    }
+
+    else if (scaledNum == 2) {
+        return "paper";
+    }
+
+    else {
+        return "scissors";
+    }
+}
